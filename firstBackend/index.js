b28c98043dccbd2de178b367557a0b82c5b4218f
Arunrai0752 <realarunrai0752@gmail.com>
2025-06-15 22:18:42 +0530

    first commit

---
+++ firstBackend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+
+    app.get("/" , (req , res)=>{
+
+        console.log("Server  Conected");
+        res.json({ message: "Hello From backend"});
+    })
+    app.listen(1200, ()=> 
+        {
+            console.log("Server Started");
+            console.log("http://localhost:1200");
+            
+            
+        })
